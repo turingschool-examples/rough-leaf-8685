b53e016cdfc3a38b2d69d530ce6966bb4e7301e1
Chris Kjolhede <ckjolhede@gmail.com>
2022-05-31 10:50:27 -0600

    write model test for sorting actors by age

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Actor, type: :model do
       it { should have_many(:actor_movies)}
       it { should have_many(:movies).through(:actor_movies)}
     end
+
+    describe 'class variables' do
+      it 'can list all actors by ascending age' do
+      actor1 = Actor.create!(name: "ActorYoungest", age: 10)
+      actor2 = Actor.create!(name: "OldestActor", age: 50)
+      actor3 = Actor.create!(name: "LowMiddleActor", age: 20)
+      actor4 = Actor.create!(name: "MiddleActor", age: 30)
+      actor5 = Actor.create!(name: "HighMiddleActor", age: 40)
+
+      expect(Actor.by_age.all).to eq([actor1, actor3, actor4, actor5, actor2])
+    end
+    end
 end
