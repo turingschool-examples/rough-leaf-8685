bcabd9fee5b474f243e6aaf8af4701c27c5785a1
Colin Reinhart <colinbreinhartgmail.com>
2022-05-31 11:53:56 -0600

    Trying to creat an actor movie to connect new actor to moive

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -5,13 +5,16 @@ class ActorsController < ApplicationController
 
   def create
     actor = Actor.create(actor_params)
-    redirect_to "/movie/#{@movie.id}"
+    actor_movie = ActorMovie.create(actor_movie_params)
+    redirect_to "/actor_movie/new"
   end
 
   private
     def actor_params
-      params.permit(:name, :age, :movie_id)
+      params.permit(:name, :age)
     end
 
-
+    def actor_movie_params
+      params.permit(:movie_id, :actor_id)
+    end
 end
