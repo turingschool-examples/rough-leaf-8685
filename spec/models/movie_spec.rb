691f466bbfe4838a6d293573de9576973e577717
Andrew King <dev.andrewking@gmail.com>
2022-05-31 12:19:39 -0500

    add model test for youngest actors

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Movie do
     it { should have_many :movie_actors}
     it { should have_many(:actors).through(:movie_actors)}
   end
+  describe 'actors_youngest_first' do
+    it 'should order by youngest age first' do
+        @studio4 = Studio.create!(name: "Warner Brothers", location: "Burbank")
+        @movie8 = @studio4.movies.create(title: "Another Warner Brothers Movie", creation_year: 2011, genre: "Romantic Comedy")
+    
+        @actor1 = Actor.create!(name: "Harrison Ford", age: 79)
+        @actor2 = Actor.create!(name: "Mark Hamill", age: 70)
+        @actor3 = Actor.create!(name: "Alec Guiness", age: 86)
+        @actor4 = Actor.create!(name: "Carrie Fisher", age: 60)
+
+        MovieActor.create(actor_id: @actor1.id, movie_id: @movie8.id)
+        MovieActor.create(actor_id: @actor2.id, movie_id: @movie8.id)
+        MovieActor.create(actor_id: @actor3.id, movie_id: @movie8.id)
+        MovieActor.create(actor_id: @actor4.id, movie_id: @movie8.id)
+
+        expect(@movie8.actors_youngest_first).to eq([@actor4, @actor2, @actor1, @actor3])
+    end
+  end
 end
