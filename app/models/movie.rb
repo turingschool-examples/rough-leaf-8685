c8e31dc6544abdc13e57a14bfc5d4159d012622a
Sage Skaff <skaffsage@gmail.com>
2022-05-31 10:14:50 -0700

    Add methods to movie model; Update movie show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def actors_average_age
+    actors.average(:age)
+  end
+
+  def actors_by_age
+    actors.order(:age)
+  end
 end
