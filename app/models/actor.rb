79e99a16df8342c9e9467e3314fbc653004c4207
Jim Riddle <jimriddle0@gmail.com>
2022-05-31 12:01:24 -0500

    got extension to pass, but using ruby.  will refactor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,20 @@
 class Actor < ApplicationRecord
   has_many :actor_movies
   has_many :movies, through: :actor_movies
+
+  def unique_coworkers
+    # binding.pry
+    unique_coworker_names = []
+    movies.each do |movie|
+      movie.actors.each do |actor|
+        unique_coworker_names << actor.name
+      end
+    end
+    unique_coworker_names.delete(self.name)
+    return unique_coworker_names.uniq.join(", ")
+    # binding.pry
+    # ApplicationPet.where(application_form_id: application_form.id, pet_id: self.id).first
+    # contestants.select(:hometown).distinct.pluck(:hometown).join ', '
+    # movies
+  end
 end
