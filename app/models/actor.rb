0301654d2ccb7854d26765da269395412af148eb
Bryce Wein <bryce.wein@gmail.com>
2022-05-31 13:24:15 -0400

    Feat: add actors #coactors instance method

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,9 @@
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+  has_many :actors, through: :movies
+
+  def coactors
+    actors.where.not(id: id).distinct.pluck(:name)
+  end
 end
