4028ba9ec9689846a9999f74009ab7c29cbd0467
Sage Skaff <skaffsage@gmail.com>
2022-05-31 09:43:16 -0700

    Add movie index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/studios', to: 'studios#index'
+
+  get '/movies/:id', to: 'movies#show'
 end
--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe 'Studio Index Page' do
   it 'lists each studio name, location, and movie titles' do
     studio1 = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
     studio2 = Studio.create!(name: 'Pixar', location: 'Emeryville')
-    movie1 = studio1.movies.create!(title: 'Tomato', creation_year: 1992)
-    movie2 = studio1.movies.create!(title: 'Potato', creation_year: 1993)
-    movie3 = studio2.movies.create!(title: 'Banana', creation_year: 1994)
-    movie4 = studio2.movies.create!(title: 'Coconut', creation_year: 1995)
+    movie1 = studio1.movies.create!(title: 'Tomato', creation_year: 1992, genre: 'Horror')
+    movie2 = studio1.movies.create!(title: 'Potato', creation_year: 1993, genre: 'Comedy')
+    movie3 = studio2.movies.create!(title: 'Banana', creation_year: 1994, genre: 'Romance')
+    movie4 = studio2.movies.create!(title: 'Coconut', creation_year: 1995, genre: 'Action/Adventure')
 
     visit '/studios'
 
