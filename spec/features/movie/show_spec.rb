7ed9f580e4d4ec35116c6af8cfbec5ae638bccfe
Sarah Hall <swatson1994@yahoo.com>
2022-05-31 12:34:33 -0400

    Add class method test and tests for order on show page

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -8,6 +8,18 @@ RSpec.describe 'Movie Show Page' do
     @fishing = @jimmy.movies.create!(title: 'Gone Fishin', creation_year: 1973, genre: 'Rom-com')
     @ark = @universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981 , genre: 'Action/Adventure')
     @et = @universal.movies.create!(title: 'ET', creation_year: 1993, genre: 'Adventure')
+
+    @jimbob = Actor.create!(name: "Jimbob Himself", age: 65)
+    @alice = Actor.create!(name: "Alice Wondra", age: 96)
+    @pete = Actor.create!(name: "Pete Downdaroad", age: 32)
+    @sam = Actor.create!(name: "Sam Nunya", age: 28)
+
+    connection1 = ActorMovie.create!(actor_id: @jimbob.id, movie_id: @fishing.id)
+    connection2 = ActorMovie.create!(actor_id: @jimbob.id, movie_id: @gocarts.id)
+    connection3 = ActorMovie.create!(actor_id: @alice.id, movie_id: @et.id)
+    connection4 = ActorMovie.create!(actor_id: @pete.id, movie_id: @gocarts.id)
+    connection5 = ActorMovie.create!(actor_id: @pete.id, movie_id: @fishing.id)
+    connection6 = ActorMovie.create!(actor_id: @sam.id, movie_id: @gocarts.id)
   end
 
   it 'can display a movies attributes on the page' do
@@ -24,5 +36,19 @@ RSpec.describe 'Movie Show Page' do
     expect(page).to have_content(@fishing.creation_year)
     expect(page).to have_content(@fishing.genre)
     expect(page).to_not have_content(@et.title)
+  end
+
+  it 'can list all actors from youngest to oldest' do
+    visit "/movies/#{@gocarts.id}"
+
+    expect(page).to have_content(@pete.name)
+    expect(page).to have_content(@sam.name)
+    expect(page).to have_content(@jimbob.name)
+
+    within '#actorList' do
+      expect(page.all('.actors')[0]).to have_content(@sam.name)
+      expect(page.all('.actors')[1]).to have_content(@pete.name)
+      expect(page.all('.actors')[2]).to have_content(@jimbob.name)
+    end
   end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Actor do
     it { should have_many :actor_movies }
     it { should have_many(:movies).through(:actor_movies) }
   end
+
+  describe 'class methods' do
+    it 'can order actors by age, youngest to oldest' do
+      jimbob = Actor.create!(name: "Jimbob Himself", age: 65)
+      alice = Actor.create!(name: "Alice Wondra", age: 96)
+      pete = Actor.create!(name: "Pete Downdaroad", age: 32)
+      sam = Actor.create!(name: "Sam Nunya", age: 28)
+
+      cast = Actor.all
+
+      expect(cast.by_youngest).to eq([sam, pete, jimbob, alice])
+    end
+  end
 end
