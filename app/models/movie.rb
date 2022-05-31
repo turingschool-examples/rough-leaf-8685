26719b32fc5dabdba33a85ea057532e2d0110dab
Zachary P <zachary.s.prince@outlook.com>
2022-05-31 12:13:07 -0400

    Add actor by age method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,9 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :actor_movies
   has_many :actors, through: :actor_movies
+
+  def actors_by_age
+    actors.order(age: :desc)
+  end
 end
 
