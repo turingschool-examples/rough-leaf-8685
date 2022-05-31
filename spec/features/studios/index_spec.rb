313c41c20ccd0a0a7a0c5458912ed29c78613474
Andrew King <dev.andrewking@gmail.com>
2022-05-31 11:51:42 -0500

    add show_spec

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe 'studio index', type: :feature do
     @movie5 = @studio3.movies.create(title: "Disney Movie", creation_year: 2005, genre: "Comedy")
     @movie6 = @studio3.movies.create(title: "Another Disney Movie", creation_year: 1993, genre: "Animated")
     @movie7 = @studio4.movies.create(title: "Warner Brothers Movie", creation_year: 2020, genre: "Documentary")
-    @movie7 = @studio4.movies.create(title: "Another Warner Brothers Movie", creation_year: 2011, genre: "Romantic Comedy")
+    @movie8 = @studio4.movies.create(title: "Another Warner Brothers Movie", creation_year: 2011, genre: "Romantic Comedy")
   end
 
   it "displays all studios and their movies" do
@@ -26,7 +26,6 @@ RSpec.describe 'studio index', type: :feature do
     expect(page).to have_content(@studio3.name)
     expect(page).to have_content(@studio4.name)
     within "#studio-0" do
-        save_and_open_page
       expect(page).to_not have_content("MGM Movie")
       expect(page).to have_content("Universal Movie")
     end
