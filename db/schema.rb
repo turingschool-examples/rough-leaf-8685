01ec7b392ee09748cf9bbb811af7dda2360bde5e
Sarah Hall <swatson1994@yahoo.com>
2022-05-31 11:16:06 -0400

    Add first migration to create actor table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_171835) do
+ActiveRecord::Schema.define(version: 2022_05_31_151113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
