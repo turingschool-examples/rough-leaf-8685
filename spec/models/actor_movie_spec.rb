e76743735a61b7d1b69d6448a13dcbf91ca7846e
Matt Deming <vballmatt44@gmail.com>
2022-05-31 11:39:59 -0600

    Finally got my relationships to work

--- spec/models/actor_movie_spec.rb
+++ spec/models/actor_movie_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ActorMovies do
+RSpec.describe ActorMovie do
   describe 'relationships' do
     it { should belong_to :movie }
     it { should belong_to :actor }
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Actor do
   describe 'relationships' do
     it { should have_many :actor_movies }
+    it { should have_many(:movies).through(:actor_movies)}
   end
 end
