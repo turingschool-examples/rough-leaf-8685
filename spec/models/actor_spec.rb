f9d2b39c80fe473e8cc094b8b52351e24b063f94
Sarah Hall <swatson1994@yahoo.com>
2022-05-31 12:52:41 -0400

    Add test for average age class method

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -17,5 +17,16 @@ RSpec.describe Actor do
 
       expect(cast.by_youngest).to eq([sam, pete, jimbob, alice])
     end
+
+    it 'can average the age of all actors' do
+      jimbob = Actor.create!(name: "Jimbob Himself", age: 65)
+      alice = Actor.create!(name: "Alice Wondra", age: 96)
+      pete = Actor.create!(name: "Pete Downdaroad", age: 32)
+      sam = Actor.create!(name: "Sam Nunya", age: 28)
+
+      cast = Actor.all
+
+      expect(cast.average_age.to_f).to eq(55.25)
+    end
   end
 end
