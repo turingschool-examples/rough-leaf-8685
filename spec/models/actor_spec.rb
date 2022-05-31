80eed1900edd7cb933ce0727211ce39bd818c2b0
Wesley Garcia <wes@alltradestech.com>
2022-05-31 09:46:07 -0700

    Add test for Actor.actors_by_age

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Actor, type: :model do
     it { should have_many :actor_movies }
     it { should have_many(:movies).through(:actor_movies) }
   end
+
+  describe "class methods" do
+    let!(:doc) { Actor.create!(name: "Christopher Lloyd", age: 83) }
+    let!(:marty) { Actor.create!(name: "Michael J. Fox", age: 60) }
+    let!(:biff) { Actor.create!(name: "Thomas F. Wilson", age: 63) }
+    let!(:indy) { Actor.create!(name: "Harrison Ford", age: 78) }
+
+    it "orders actors by age" do
+      expect(Actor.actors_by_age).to eq([marty, biff, indy, doc])
+    end
+  end
+
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe Movie do
   end
 
   describe "instance methods" do
-    it "orders actors in a movie by age" do
-      let!(:universal) { Studio.create!(name: "Universal Studios", location: "Hollywood") }
-      let!(:bttf) { Movie.create!(title: "Back to the Future", creation_year: 1985, genre: "Science Fiction", studio: universal) }
-      let!(:doc) { Actor.create!(name: "Christopher Lloyd", age: 83) }
-      let!(:marty) { Actor.create!(name: "Michael J. Fox", age: 60) }
-      let!(:biff) { Actor.create!(name: "Thomas F. Wilson", age: 63) }
-      let!(:actor_movie3) { ActorMovie.create!(actor: marty, movie: bttf) }
-      let!(:actor_movie4) { ActorMovie.create!(actor: doc, movie: bttf) }
-      let!(:actor_movie5) { ActorMovie.create!(actor: biff, movie: bttf) }
+    let!(:universal) { Studio.create!(name: "Universal Studios", location: "Hollywood") }
+    let!(:bttf) { Movie.create!(title: "Back to the Future", creation_year: 1985, genre: "Science Fiction", studio: universal) }
+    let!(:doc) { Actor.create!(name: "Christopher Lloyd", age: 83) }
+    let!(:marty) { Actor.create!(name: "Michael J. Fox", age: 60) }
+    let!(:biff) { Actor.create!(name: "Thomas F. Wilson", age: 63) }
+    let!(:actor_movie3) { ActorMovie.create!(actor: marty, movie: bttf) }
+    let!(:actor_movie4) { ActorMovie.create!(actor: doc, movie: bttf) }
+    let!(:actor_movie5) { ActorMovie.create!(actor: biff, movie: bttf) }
 
+    it "orders actors in a movie by age" do
       expect(bttf.actors_by_age).to eq([marty, biff, doc])
     end
   end
