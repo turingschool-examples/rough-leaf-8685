8c1ba55302e8f7addd9face964e2371dc8d789cb
Cory Bethune <corybbethune@gmail.com>
2022-05-31 10:29:33 -0700

    Passes test

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -39,4 +39,25 @@ RSpec.describe 'movie show page' do
     expect(actor2.name).to appear_before(actor1.name)
     expect(page).to_not have_content(actor3.name)
   end
+
+  it 'has a form that lets you add an actor to the movie' do
+    studio1 = Studio.create!(name: "Lucas Arts", location: "California")
+    studio2 = Studio.create!(name: "Paramount", location: "New York")
+    movie1 = studio1.movies.create!(title: "Wall-E", creation_year: 2009, genre: "Animation")
+    movie2 = studio1.movies.create!(title: "Pocahontas", creation_year: 1994, genre: "Action")
+    movie3 = studio2.movies.create!(title: "Spirited Away", creation_year: 2007, genre: "Mystery")
+    movie4 = studio2.movies.create!(title: "Princess Mononoke", creation_year: 1997, genre: "Drama")
+    actor1 = movie1.actors.create!(name: "Sandy", age: 80)
+    actor2 = movie1.actors.create!(name: "Cory", age: 20)
+    actor3 = movie2.actors.create!(name: "Bill", age: 40)
+
+    visit "movies/#{movie1.id}"
+
+    expect(page).to_not have_content 'Bill'
+
+    fill_in 'Actor', with: 'Bill'
+    click_button 'Search'
+
+    expect(page).to have_content 'Bill'
+  end
 end
