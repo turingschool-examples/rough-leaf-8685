fb5f8eeb0b64c7b7f170c796bccb720fc4b85ecf
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-31 10:07:23 -0600

    Add model method for ordering by age and show page test for ordering

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -5,6 +5,14 @@ RSpec.describe 'Movie Show Page' do
 
   let!(:movie2) { studio2.movies.create(title: 'Spirited Away', creation_year: 2002, genre: 'Drama') }  
 
+  let!(:actor1) { Actor.create!(name: 'Daveigh Chase', age: 32)}
+  let!(:actor2) { Actor.create!(name: 'Miyu Irino', age: 51)}
+  let!(:actor3) { Actor.create!(name: 'Mari Natsuki', age: 48)}
+
+  let!(:movie_actor1) { MovieActor.create!(actor_id: actor1.id, movie_id: movie2.id)}
+  let!(:movie_actor2) { MovieActor.create!(actor_id: actor2.id, movie_id: movie2.id)}
+  let!(:movie_actor3) { MovieActor.create!(actor_id: actor3.id, movie_id: movie2.id)}
+
   describe 'user story 2' do 
     it 'has movie attributes' do 
       visit "movies/#{movie2.id}"
@@ -12,6 +20,16 @@ RSpec.describe 'Movie Show Page' do
       expect(page).to have_content(movie2.title)
       expect(page).to have_content(movie2.creation_year)
       expect(page).to have_content(movie2.genre)
+    end
+
+    it 'can list the actors from youngest to oldest' do 
+      visit "movies/#{movie2.id}"
+
+      expect(page).to have_content(actor1.name)
+      expect(page).to have_content(actor2.name)
+      expect(page).to have_content(actor3.name)
+      expect("Daveigh Chase").to appear_before("Mari Natsuki")
+      expect("Mari Natsuki").to appear_before("Miyu Irino")
     end
   end
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Actor do
     it { should have_many :movie_actors}
     it { should have_many(:movies).through(:movie_actors)}
   end
+
+  let!(:actor1) { Actor.create!(name: 'Daveigh Chase', age: 32)}
+  let!(:actor2) { Actor.create!(name: 'Miyu Irino', age: 51)}
+  let!(:actor3) { Actor.create!(name: 'Mari Natsuki', age: 48)}
+
+  describe 'class methods' do 
+    describe 'order youngest to oldest' do 
+      it '#order_by_age' do
+        expect(Actor.order_by_age).to eq([actor1, actor3, actor2])
+      end
+
+    end
+  end
 end
