47534c3034cf12b89ff88953530b903ad5a9cfc1
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-31 09:20:24 -0600

    Feat: test and create relationships

---
+++ spec/models/actor_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper' 
+
+RSpec.describe Actor, type: :model do
+
+    describe "relationships" do
+        it { should have_many :actor_movies }
+        it { should have_many(:movies).through(:actor_movies) }
+    end
+
+end
