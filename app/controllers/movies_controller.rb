632f15a97e4a3eac547393c8b9cff932836740d6
Sage Skaff <skaffsage@gmail.com>
2022-05-31 10:59:56 -0700

    Comment out unfinished code

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,6 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  # def update; end
 end
--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -31,4 +31,22 @@ RSpec.describe 'Movie Show Page' do
     expect(page).to_not have_content('Strawberry')
     expect(page).to have_content('33')
   end
+
+  xit 'has form for adding an actor to the movie' do
+    studio1 = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+    movie1 = studio1.movies.create!(title: 'Tomato', creation_year: 1992, genre: 'Horror')
+    actor1 = Actor.create!(name: 'Apple', age: 27)
+    actor2 = Actor.create!(name: 'Kiwi', age: 43)
+    actor3 = Actor.create!(name: 'Blueberry', age: 29)
+    actor4 = Actor.create!(name: 'Strawberry', age: 31)
+    MovieActor.create!(movie: movie1, actor: actor1)
+    MovieActor.create!(movie: movie1, actor: actor2)
+    MovieActor.create!(movie: movie1, actor: actor3)
+    visit "/movies/#{movie1.id}"
+    expect(page).to_not have_content('Strawberry')
+    fill_in 'Add actor to movie:', with: 'Strawberry'
+    click_on 'Submit'
+    expect(current_path).to eq("/movies/#{movie1.id}")
+    expect(page).to have_content('Strawberry')
+  end
 end
