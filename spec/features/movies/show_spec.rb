08e165893ec7f8f184556e0d39f9453e775ee6fa
Zachary P <zachary.s.prince@outlook.com>
2022-05-31 12:23:39 -0400

    Remove debugger entry point

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'movies show page' do
-  xit 'shows the movie title, creation year, and genre' do
+  it 'shows the movie title, creation year, and genre' do
     marvel = Studio.create!(name: 'Marvel', location: 'Denver, CO')
 
     iron_man = marvel.movies.create!(title: 'Iron Man', creation_year: '2008', genre: 'Action')
@@ -35,7 +35,37 @@ RSpec.describe 'movies show page' do
     am4 = ActorMovie.create!(actor_id: chris_h.id, movie_id: captain_america.id)
 
     visit "/movies/#{iron_man.id}"
+    expect(page).to have_content('Iron Man')
+    expect(page).to have_content('2008')
+    expect(page).to have_content('Action')
 
+    within('div#actors') do
+      expect('James Cameron').to appear_before('Sarah Jessica Parker')
+      expect('Sarah Jessica Parker').to appear_before('Robert Downey Jr.')
+      expect(page).to_not have_content('Chris Hemsworth')
+    end
+  end
+
+  it 'shows the average age of actors' do
+    marvel = Studio.create!(name: 'Marvel', location: 'Denver, CO')
+
+    iron_man = marvel.movies.create!(title: 'Iron Man', creation_year: '2008', genre: 'Action')
+    captain_america = marvel.movies.create!(title: 'Captain America', creation_year: '2010', genre: 'Action')
+    thor = marvel.movies.create!(title: 'Thor', creation_year: '2011', genre: 'Action')
+
+    robertdj = Actor.create!(name: 'Robert Downey Jr.', age: 45)
+    sarahjp = Actor.create!(name: 'Sarah Jessica Parker', age: 44)
+    jamesc = Actor.create!(name: 'James Cameron', age: 43)
+
+    chris_h = Actor.create!(name: 'Chris Hemsworth', age: 35)
+
+    am1 = ActorMovie.create!(actor_id: robertdj.id, movie_id: iron_man.id)
+    am2 = ActorMovie.create!(actor_id: sarahjp.id, movie_id: iron_man.id)
+    am3 = ActorMovie.create!(actor_id: jamesc.id, movie_id: iron_man.id)
+
+    am4 = ActorMovie.create!(actor_id: chris_h.id, movie_id: captain_america.id)
+
+    visit "/movies/#{iron_man.id}"
     expect(page).to have_content('Iron Man')
     expect(page).to have_content('2008')
     expect(page).to have_content('Action')
