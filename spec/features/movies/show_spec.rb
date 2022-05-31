8f41a949d2be194f490c10f14b56fc83ad9727fc
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-05-31 11:50:15 -0600

    Feat: add average age to movie show view

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,22 +1,3 @@
-# As a user,
-# When I visit a movie's show page.
-# I see the movie's title, creation year, and genre,
-# and a list of all its actors from youngest to oldest.
-# And I see the average age of all of the movie's actors
-
-
-# Add an Actor to a Movie
-#
-# As a user,
-# When I visit a movie show page,
-# I do not see any actors listed that are not part of the movie
-# And I see a form to add an actor to this movie
-# When I fill in the form with the name of an actor that exists in the database
-# And I click submit
-# Then I am redirected back to that movie's show page
-# And I see the actor's name is now listed
-# (You do not have to test for a sad path, for example if the name submitted is not an existing actor)
-
 require 'rails_helper'
 
 RSpec.describe 'movie show page' do
@@ -43,7 +24,39 @@ RSpec.describe 'movie show page' do
 
       expect(page).to have_content('John Goodman')
       expect(page).to have_content('Eddie Murphey')
+
+      expect('John Goodman').to appear_before('Eddie Murphey')
+      expect(page).to have_content('Average Age of Actors: 50')
   end
+
+  # Add an Actor to a Movie
+  #
+  # As a user,
+  # When I visit a movie show page,
+  # I do not see any actors listed that are not part of the movie
+  # And I see a form to add an actor to this movie
+  # When I fill in the form with the name of an actor that exists in the database
+  # And I click submit
+  # Then I am redirected back to that movie's show page
+  # And I see the actor's name is now listed
+  # (You do not have to test for a sad path, for example if the name submitted is not an existing actor)
+
+  
+  it "displays the movie's attributes, movie titles, actors, and average afe of all actors for that movie" do
+    visit movie_path(movie2)
+
+    expect(page).to have_content('Title: Monsters, Inc.')
+    expect(page).to have_content('Creation Year: 2001')
+    expect(page).to have_content('Genre: Animation')
+    expect(page).to_not have_content('Title: Rock n Rolla')
+
+    expect(page).to have_content('John Goodman')
+    expect(page).to have_content('Eddie Murphey')
+
+    expect('John Goodman').to appear_before('Eddie Murphey')
+    expect(page).to have_content('Average Age of Actors: 50')
+  end
+
 end
 
 
