8ef71cc8957c0710af621c1ed859d751c291ad0c
Bryce Wein <bryce.wein@gmail.com>
2022-05-31 11:34:01 -0400

    Test: story 1 test

---
+++ spec/features/studios/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Studio, type: :feature do
+  describe '#index' do
+    it 'display each studios information and movies associated with each' do
+      studio1 = Studio.create!(name: 'Paramount', location: 'Hollywood')
+      studio2 = Studio.create!(name: 'Universal', location: 'Houston')
+      movie1 = Movie.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure',
+                             studio_id: studio1.id)
+      movie2 = Movie.create!(title: 'Fast 5', creation_year: 2011, genre: 'Action/Adventure', studio_id: studio2.id)
+
+      visit studios_path
+
+      within "#studio-#{studio1.id}" do
+        expect(page).to have_content(studio1.name)
+        expect(page).to have_content(studio1.location)
+        expect(page).to have_content(movie1.title)
+      end
+    end
+  end
+end
