822151f0ac5ce7c48320526a8e93c0a664bcf0e7
Bryce Wein <bryce.wein@gmail.com>
2022-05-31 12:13:37 -0400

    Feat: add movie model methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def age_sorted_actors
+    actors.order(:age)
+  end
+
+  def actors_average_age
+    actors.average(:age)
+  end
 end
