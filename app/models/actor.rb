f238d990037ceff222b61a393a556b0770030735
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-31 11:26:18 -0600

    Feat: create coactors method in actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,4 +3,9 @@ class Actor < ApplicationRecord
   has_many :actor_movies
   has_many :movies, through: :actor_movies
 
+  def coactors
+    actor_movie_ids = Movie.joins(:actors).where("actors.id = #{self.id}").pluck(:id)
+    actor_ids = ActorMovie.where(movie_id: actor_movie_ids).pluck(:actor_id)
+    actors = Actor.where(id: actor_ids).where.not(id: self.id)
+  end
 end
