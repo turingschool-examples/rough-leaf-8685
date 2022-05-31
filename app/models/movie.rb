b6250b9bb0554ebde7b655504e3a212d7b0592be
Cory Bethune <corybbethune@gmail.com>
2022-05-31 09:47:49 -0700

    Create actor sort by age method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :actor_movies
   has_many :actors, through: :actor_movies
+
+  def actor_sort_by_age
+    actors.order(age: :asc)
+  end
 end
