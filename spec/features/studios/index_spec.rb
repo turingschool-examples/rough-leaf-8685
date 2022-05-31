5e2b776dd07c5c96d853eb6ab7ff284e14706959
Deannah Burke <deannah@bierstadtlager.com>
2022-05-31 10:47:24 -0600

    Finish user story 1, within block tests failing but displaying correctly in server

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -23,4 +23,30 @@ RSpec.describe 'the studio index page', type: :feature do
     expect(page).to_not have_content("Columbia Pictures")
     expect(page).to_not have_content("San Francisco")
   end
+
+  it 'displays each studios movies underneath' do
+    universal = Studio.create!(name: "Universal", location: "Hollywood")
+    warner_bros = Studio.create!(name: "Warner Brothers", location: "Los Angeles")
+
+    raiders = universal.movies.create!(title: "Raiders of the Lost Ark", creation_year: 1981, genre: "Action/Adventure")
+    indiana = universal.movies.create!(title: "Indiana Jones", creation_year: 1979, genre: "Action/Adventure")
+    jurassic = warner_bros.movies.create!(title: "Jurassic Park", creation_year: 1993, genre: "Sci-Fi")
+    true_romance = warner_bros.movies.create!(title: "True Romance", creation_year: 1995, genre: "Romance")
+
+    visit "/studios"
+
+      within "#universal-#{universal.id}" do
+        expect(page).to have_content("Raiders of the Lost Ark")
+        expect(page).to have_content("Indiana Jones")
+        expect(page).to_not have_content("Jurassic Park")
+        expect(page).to_not have_content("True Romance")
+      end
+
+      within "#warner_bros-#{warner_bros.id}" do
+        expect(page).to have_content("Jurassic Park")
+        expect(page).to have_content("True Romance")
+        expect(page).to_not have_content("Raiders of the Lost Ark")
+        expect(page).to_not have_content("Indiana Jones")
+      end
+  end
 end
