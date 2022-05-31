add12bef8c0a5bff50d13a3f5f2e9192203cf9f3
Sage Skaff <skaffsage@gmail.com>
2022-05-31 09:11:58 -0700

    Add studio index page

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Studio Index Page' do
     studio2 = Studio.create!(name: 'Pixar', location: 'Emeryville')
     movie1 = studio1.movies.create!(title: 'Tomato', creation_year: 1992)
     movie2 = studio1.movies.create!(title: 'Potato', creation_year: 1993)
-    movie3 = studio2.movies.create!(title: 'Pinapple', creation_year: 1994)
+    movie3 = studio2.movies.create!(title: 'Banana', creation_year: 1994)
     movie4 = studio2.movies.create!(title: 'Coconut', creation_year: 1995)
 
     visit '/studios'
@@ -16,14 +16,14 @@ RSpec.describe 'Studio Index Page' do
       expect(page).to have_content('Hollywood')
       expect(page).to have_content('Tomato')
       expect(page).to have_content('Potato')
-      expect(page).to_not have_content('Pineapple')
+      expect(page).to_not have_content('Banana')
       expect(page).to_not have_content('Pixar')
     end
 
     within('#studio-1') do
       expect(page).to have_content('Pixar')
       expect(page).to have_content('Emeryville')
-      expect(page).to have_content('Pineapple')
+      expect(page).to have_content('Banana')
       expect(page).to have_content('Coconut')
       expect(page).to_not have_content('Universal Studios')
       expect(page).to_not have_content('Tomato')
