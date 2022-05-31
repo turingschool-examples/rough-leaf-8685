450ad0301121aa276ed33d1b273908a5253fc45a
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-31 11:32:35 -0600

    Feat: US3, Add actor to movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,10 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
+    @movie_actors = if !params[:actors_by_name].nil? && params[:actors_by_name] != ""
+                      Actor.where('lower(name) LIKE?', "%#{params[:actors_by_name].downcase}%")
+                    else
+                      []
+                    end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/studios', to: 'studios#index'
 
   get '/movies/:id', to: 'movies#show'
+
+  post '/actor_movies', to: 'actor_movies#create'
 end
