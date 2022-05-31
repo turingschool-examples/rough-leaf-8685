5fff326fbfb0c5528de9daaf2cb2b7be0663203a
Stephen Wilkens <stephenwilkens@gmail.com>
2022-05-31 09:47:57 -0600

    Add studios index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-  Character.create(name: 'Luke', movie: movies.first)
+  #movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  #Character.create(name: 'Luke', movie: movies.first)
+
+@studio_1 = Studio.create!(name: 'Paramount', location: "Hollywoo")
+@studio_2 = Studio.create!(name: 'Pixar', location: "Orlando")
+@sw = @studio_1.movies.create!(title: "Star Wars", creation_year: 1984, genre: "Sci-fi" )
+@lotr = @studio_1.movies.create!(title: "Lord of the Rings", creation_year: 2001, genre: "Fantasy" )
+@nemo = @studio_2.movies.create!(title: "Finding Nemo", creation_year: 2004, genre: "Animation" )
--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe 'Studio Index Page' do
   before :each do
     @studio_1 = Studio.create!(name: 'Paramount', location: "Hollywoo")
-    @studio_1 = Studio.create!(name: 'Pixar', location: "Orlando")
+    @studio_2 = Studio.create!(name: 'Pixar', location: "Orlando")
     @sw = @studio_1.movies.create!(title: "Star Wars", creation_year: 1984, genre: "Sci-fi" )
     @lotr = @studio_1.movies.create!(title: "Lord of the Rings", creation_year: 2001, genre: "Fantasy" )
     @nemo = @studio_2.movies.create!(title: "Finding Nemo", creation_year: 2004, genre: "Animation" )
@@ -11,8 +11,7 @@ RSpec.describe 'Studio Index Page' do
 
   it 'displays each studios name location and the titles of all its movies' do
     visit "/studios"
-
-    within "#studio-#{studio_1.id}" do
+    within "#studio-#{@studio_1.id}" do
       expect(page).to have_content('Paramount')
       expect(page).to have_content('Hollywoo')
       expect(page).to have_content('Star Wars')
@@ -21,7 +20,7 @@ RSpec.describe 'Studio Index Page' do
       expect(page).to_not have_content('Finding Nemo')
     end
 
-    within "#studio-#{studio_2.id}" do
+    within "#studio-#{@studio_2.id}" do
       expect(page).to have_content('Pixar')
       expect(page).to have_content('Orlando')
       expect(page).to have_content('Finding Nemo')
